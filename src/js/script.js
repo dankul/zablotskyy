8f4468de72d2134c973378a49075ca8439cbdce5
Kulakovsky Daniil <dk@empat.tech>
2023-07-10 17:40:56 +0300

    change caries scss to css

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,18 @@
-const swiper = new Swiper('.swiper', {
-    slidesPerView: 1,
-    // autoHeight: true,
-    navigation: {
-        nextEl: '.button-next',
-        prevEl: '.button-prev',
-    },
-    breakpoints: {
-        768: {
-            slidesPerView: 'auto',
+if (document.querySelector('.swiper')) {
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 1,
+        // autoHeight: true,
+        navigation: {
+            nextEl: '.button-next',
+            prevEl: '.button-prev',
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 'auto',
+            }
         }
-    }
-});
+    });
+}
 
 if (document.querySelector('.news__switcher')) {
     document.querySelector('.news__switcher').querySelectorAll('p').forEach((el) => {
@@ -54,6 +56,18 @@ if (document.querySelector('.important__accordion')) {
     document.querySelectorAll('.important__accordion').forEach((accordion) => {
         accordion.addEventListener('click', () => {
             accordion.classList.toggle('active');
+        })
+    })
+}
+
+if(document.querySelector('.appointment__tabs')) {
+    document.querySelectorAll('.appointment__tab').forEach((tab) => {
+        tab.addEventListener('click', () => {
+            document.querySelector('.appointment__tab.active').classList.remove('active')
+            tab.classList.add('active')
+
+            document.querySelector('.appointment__content.active').classList.remove('active')
+            document.querySelector('.js-' + tab.dataset.tab).classList.add('active')
         })
     })
 }
